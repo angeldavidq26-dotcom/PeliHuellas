a0e63162d9dc5752e837363e897f0cba640c299f
mateop26 <mateop2626@gmail.com>
2026-09-14 15:46:34 -0500

    Logo y Rutas

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Facades\Route;
 
 
 
+// Route::view('/', 'index')->name('home');
 Route::view('/', 'index')->name('home');
 Route::view('/panel-fundacion', 'Panel_fundacion')->name('panel.fundacion');
 
